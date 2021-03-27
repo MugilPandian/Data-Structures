3c5c91f67f234f8be1e0c246ff37882d272adf9f
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-03-27 10:16:21 +0530

    BST

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,85 +1,89 @@
 import java.util.*;
-public class SinglyLinkedList{
+
+public class SinglyLinkedList {
     int value;
     SinglyLinkedList next;
-    public SinglyLinkedList(int a){
-        value=a;
-        next=null;
+
+    public SinglyLinkedList(int a) {
+        value = a;
+        next = null;
     }
 }
-class Main{
-    public SinglyLinkedList head=null;
-    public SinglyLinkedList tail=null;
-    public void insert(int a){
+
+class Main {
+    public SinglyLinkedList head = null;
+    public SinglyLinkedList tail = null;
+
+    public void insert(int a) {
         SinglyLinkedList ll = new SinglyLinkedList(a);
-        if(head==null){
-            head=ll;
-            tail=ll;
-        }
-        else{
-            tail.next=ll;
-            tail=ll;
+        if (head == null) {
+            head = ll;
+            tail = ll;
+        } else {
+            tail.next = ll;
+            tail = ll;
         }
     }
-    public void search(int a){
-        SinglyLinkedList temp=head;
-        if(head==null){
+
+    public void search(int a) {
+        SinglyLinkedList temp = head;
+        if (head == null) {
             System.out.println("List is Empty");
-        }
-        else{
-            int f=0;
-            while(temp!=null){
-                if(temp.value==a){
-                    f=1;
+        } else {
+            int f = 0;
+            while (temp != null) {
+                if (temp.value == a) {
+                    f = 1;
                     break;
                 }
-                temp=temp.next;
+                temp = temp.next;
             }
-            if(f==1)
+            if (f == 1)
                 System.out.println("Element is found");
             else
                 System.out.println("Element is NOT found");
         }
     }
-    public void display(){
-        SinglyLinkedList temp=head;
-        if(head==null){
+
+    public void display() {
+        SinglyLinkedList temp = head;
+        if (head == null) {
             System.out.println("List is Empty");
             return;
-        }
-        else{
+        } else {
             System.out.println("The elements are:");
-            while(temp!=null){
-                System.out.println(temp.value+" ");
-                temp=temp.next;
+            while (temp != null) {
+                System.out.println(temp.value + " ");
+                temp = temp.next;
             }
         }
     }
-    public void insertsorted(int a){
-        SinglyLinkedList temp=head;
-        if(head==null){
+
+    public void insertsorted(int a) {
+        SinglyLinkedList temp = head;
+        if (head == null) {
             System.out.println("List is Empty");
             return;
-        }
-        else{
-            while(temp!=null){
-                if(temp.value>a){
-                    temp.next.value=a;
+        } else {
+            while (temp != null) {
+                if (temp.value > a) {
+                    temp.next.value = a;
                 }
-                temp=temp.next;
+                temp = temp.next;
             }
         }
     }
-    public void delete(int data){
+
+    public void delete(int data) {
         SinglyLinkedList curr = head.next;
         SinglyLinkedList prev = head;
-        if(head.value==data)
+        if (head.value == data)
             head = head.next;
-        else{
-            while(curr!=null){
-                if(curr.value==data){
-                    if(tail.value==data){
-                        tail=prev;
+        else {
+            while (curr != null) {
+                if (curr.value == data) {
+                    if (tail.value == data) {
+                        tail = prev;
                         tail.next = null;
                         break;
                     }
@@ -87,50 +91,50 @@ class Main{
                     break;
                 }
                 prev = curr;
-                curr=curr.next;
+                curr = curr.next;
             }
         }
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Main list = new Main();
         Scanner sc = new Scanner(System.in);
-        int ch=0;
-        while(ch!=6){
+        int ch = 0;
+        while (ch != 6) {
             System.out.println("1.Create 2.Insert 3.Delete 4.Display 5.Search 6.Exit");
             System.out.println("Enter a choice:");
-            ch=sc.nextInt();
-            switch(ch){
-                case 1:
+            ch = sc.nextInt();
+            switch (ch) {
+            case 1:
                 System.out.println("Enter the no of values:");
                 int num = sc.nextInt();
                 System.out.println("Enter the values:");
-                for(int i=0;i<num;i++)
+                for (int i = 0; i < num; i++)
                     list.insert(sc.nextInt());
                 break;
-                case 2:
-                    System.out.println("Insert a value:");
-                    list.insert(sc.nextInt());
-                    break;
-                case 3:
-                    System.out.println("Enter the number to be deleted: ");
-                    int del = sc.nextInt();
-                    list.delete(del);
-                    break;
-                case 4:
-                    list.display();
-                    break;
-                case 5:
-                    System.out.println("Enter the element to be searched:");
-                    list.search(sc.nextInt());
-                    break;
-                case 6:
-                    System.out.println("Exited");
-                    break;
-                default:
-                    System.out.println("Invalid Choice");
+            case 2:
+                System.out.println("Insert a value:");
+                list.insert(sc.nextInt());
+                break;
+            case 3:
+                System.out.println("Enter the number to be deleted: ");
+                int del = sc.nextInt();
+                list.delete(del);
+                break;
+            case 4:
+                list.display();
+                break;
+            case 5:
+                System.out.println("Enter the element to be searched:");
+                list.search(sc.nextInt());
+                break;
+            case 6:
+                System.out.println("Exited");
+                break;
+            default:
+                System.out.println("Invalid Choice");
             }
         }
         sc.close();
     }
 }
-
